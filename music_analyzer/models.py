b6cce206f5e320c94eb32a6030fccbf9cf05bd1a
s1f10250184 <s1f102501844@iniad.org>
2025-12-18 17:58:47 +0900

    モデルの部分をいじってこのリンク踏んだ後の状態を保存して、次行ったときにスムーズに行けるようにする。更新ボタンを追加も必要

--- music_analyzer/models.py
+++ music_analyzer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
+# Create your models here.
+class SpotifyUser(models.Model):
+    spotify_user_id = models.CharField(max_length=64, unique=True)
+    display_name = models.CharField(max_length=255, blank=True, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return self.display_name or self.spotify_user_id
 
