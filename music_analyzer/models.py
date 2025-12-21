5a3537b85ba945eff1ca37b32a2ed4cf9386b83b
s1f10250184 <s1f102501844@iniad.org>
2025-12-21 16:27:14 +0900

    一旦チーム演習の状態に戻したけどこれむずいな、というかなんかチーム演習のほうが個人に干渉しはじめてる？runserverやったら結果が求めていたのと違う形になっていたから一旦戻る、あとブランチ切ろう

--- music_analyzer/models.py
+++ music_analyzer/models.py
@@ -1,23 +1,3 @@
 from django.db import models
-from django.utils import timezone
 
-# Create your models here.
-class SpotifyUser(models.Model):
-    spotify_user_id = models.CharField(max_length=64, unique=True)
-    access_token = models.CharField(max_length=255, blank=True, default="")
-    refresh_token = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.spotify_user_id
-    
-class TopTracksSnapshot(models.Model):
-    user = models.ForeignKey("SpotifyUser", on_delete=models.CASCADE)
-    term = models.CharField(max_length=16)
-    limit = models.IntegerField(default=20)
-    fetched_at = models.DateTimeField(default=timezone.now)
-    data = models.JSONField(default=dict)
-
-class Meta:
-    ordering = ["-fetched_at"]
 
--- music_analyzer/views.py
+++ music_analyzer/views.py
@@ -3,10 +3,6 @@ from django.http import HttpResponse
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
-from spotipy.exceptions import SpotifyException
-from django.views.decorators.http import require_POST
-from .models import SpotifyUser, TopTracksSnapshot
-
 
 
 # Create your views here.
@@ -23,31 +19,31 @@ def spotify_callback(request):
     code = request.GET.get("code")
     token_info = sp_oauth.get_access_token(code)
     access_token = token_info["access_token"]
-    refresh_token = token_info.get("refresh_token")
     sp = spotipy.Spotify(auth=access_token)
 
     limit = int(request.session.get("limit", 20))
     profile = sp.current_user()
-    spotify_user_id = profile["id"]
 
-    user = SpotifyUser.objects.filter(spotify_user_id=spotify_user_id).first()
-    if user:
-        user.access_token = access_token
-        if refresh_token:
-            user.refresh_token = refresh_token
-        user.save()
+    term = request.session.get("time_range", "short_term")
+    top_tracks = sp.current_user_top_tracks(limit=limit, time_range=term)
+    items = top_tracks["items"]
+    for t in items:
+        track_name = t["name"]
+        artist_name = t["artists"][0]["name"]
+        print(f"{track_name} {artist_name}")
+
+    if term == "short_term":
+        term_label = "1か月"
+    elif term == "medium_term":
+        term_label = "6か月"
     else:
-        if not refresh_token:
-            return redirect("spotify_login")
-        SpotifyUser.objects.create(
-            spotify_user_id=spotify_user_id,
-            access_token=access_token,
-            refresh_token=refresh_token,
-        )
-    
-    request.session["spotify_user_id"] = spotify_user_id
+        term_label = "1年"
 
-    return redirect("result")
+    return render(request, "music_analyzer/result.html", {
+        "tracks": items,
+        "limit": limit,
+        "term_label":term_label,
+    })
 
 
 
@@ -73,77 +69,3 @@ def spotify_login(request):
     request.session["time_range"] = term
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
-
-def _current_user_row(request):
-    spotify_user_id = request.session.get("spotify_user_id")
-    if not spotify_user_id:
-        return None
-    return SpotifyUser.objects.filter(spotify_user_id=spotify_user_id).first()
-
-def _spotify_client(user: SpotifyUser) -> spotipy.Spotify:
-    sp = spotipy.Spotify(auth=user.access_token)
-    try:
-        sp.current_user()
-        return sp
-    except SpotifyException as e:
-        if getattr(e, "http_status", None) == 401:
-            new = sp_oauth.refresh_access_token(user.refresh_token)
-            user.access_token = new["access_token"]
-            user.save(update_fields=["access_token"])
-            return spotipy.Spotify(auth=user.access_token)
-        raise
-
-@require_POST
-def refresh_top(request):
-    user = _current_user_row(request)
-    if not user:
-        return redirect("spotify_login")
-    
-    sp = _spotify_client(user)
-
-    term = request.session.get("time_range", "short_term")
-    limit = int(request.session.get("limit", 20))
-
-    top_tracks = sp.current_user_top_tracks(limit=limit, time_range=term)
-
-    tracks = []
-    for t in top_tracks["items"]:
-        tracks.append({
-            "name": t["name"],
-            "article": t["article"],
-            "album": {"images": t["album"]["images"]},
-            "external_urls": t["external_urls"]
-        })
-
-    TopTracksSnapshot.objects.create(
-        user=user,
-        term=term,
-        limit=limit,
-        dat={"tracks": tracks},
-    )
-
-    return redirect("result")
-
-def result(request):
-    user = _current_user_row(request)
-    if not user:
-        return redirect("spotify_login")
-    
-    term = request.session.get("time_range", "short_term")
-    limit = int(request.session.get("limit", 20))
-
-    snap = (TopTracksSnapshot.objects
-            .filter(user=user, term=term, limit=limit)
-            .order_by("-fetched_at")
-            .first())
-    
-    tracks = snap.data.get("tracks", []) if snap else []
-
-    term_label = "1か月" if term == "short=term" else "6か月" if term == "medium_term" else "1年"
-
-    return render(request, "music_analyzer/result.html", {
-        "tracks": tracks,
-        "limit" : limit,
-        "term_label": term_label,
-        "need_refresh": (snap is None),
-    })
