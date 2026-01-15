f41747f971b8f676facc2a13dc123d0ec35deef8
s1f10250184 <s1f102501844@iniad.org>
2026-01-15 15:33:10 +0900

    安全ではないのか？一旦デプロイ用

--- music_analyzer/views.py
+++ music_analyzer/views.py
@@ -10,11 +10,12 @@ from .models import SpotifyUser, TopTrackSnapshot
 
 
 # OAuth設定
-sp_oauth = SpotifyOAuth(
-    client_id=os.environ.get("SPOTIFY_CLIENT_ID"),
-    client_secret=os.environ.get("SPOTIFY_CLIENT_SECRET"),
-    redirect_uri = os.environ.get("SPOTIFY_REDIRECT_URI"),
-    scope="user-top-read user-read-recently-played",
+def get_sp_oauth():
+    return SpotifyOAuth(
+        client_id=os.environ.get("SPOTIFY_CLIENT_ID"),
+        client_secret=os.environ.get("SPOTIFY_CLIENT_SECRET"),
+        redirect_uri = os.environ.get("SPOTIFY_REDIRECT_URI"),
+        scope="user-top-read user-read-recently-played",
 )
 
 
@@ -37,6 +38,7 @@ def spotify_login(request):
         request.session.setdefault("time_range", "short_term")
     if request.session.get("spotify_user_id"):
         return redirect("result")
+    sp_oauth = get_sp_oauth()
     return redirect(sp_oauth.get_authorize_url())
 
 
@@ -59,6 +61,7 @@ def _spotify_client(user: SpotifyUser) -> spotipy.Spotify:
         return sp
     except SpotifyException as e:
         if getattr(e, "http_status", None) == 401 and user.refresh_token:
+            sp_oauth = get_sp_oauth
             new = sp_oauth.refresh_access_token(user.refresh_token)
             user.access_token = new["access_token"]
             user.save(update_fields=["access_token"])
@@ -87,7 +90,7 @@ def _save_snapshot(user: SpotifyUser, items: list, term: str, limit: int):
 def spotify_callback(request):
     code = request.GET.get("code")
     token_info = sp_oauth.get_access_token(code)
-
+    sp_oauth = get_sp_oauth
     access_token = token_info["access_token"]
     refresh_token = token_info.get("refresh_token")
 
